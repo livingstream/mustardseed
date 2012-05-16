f8165b59f914167431b4965a9abaca2361e328f1
Kun Li <kli@cise.ufl.edu>
2012-05-16 11:02:31 -0400

    up

--- mcmc.cpp
+++ mcmc.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <hash_map>
+#include <sstream>
+#include <set>
 using namespace std;
 #define Nmen 29
 struct mentions {
@@ -12,14 +13,42 @@ struct mentions {
 };
 struct entity {
 	int id; // An unique identifier for the entity (in consequential)
-         
+        set<int>mentions;         
 };
+
+bool affinity(string str1, string str2){
+  int sumAff=0;
+  str1="abced";
+  str2="abced";
+  // match the the prefix with 1 character
+  if(str1.substr(0,1).compare(str2.substr(0,1))==0) sumAff+=2; 
+  if(str1.substr(0,2).compare(str2.substr(0,2))==0) sumAff+=2;
+  if(str1.substr(0,3).compare(str2.substr(0,3))==0) sumAff+=3;
+  if(str1.find(str2)!=string::npos) sumAff+=10;
+  if(str2.find(str1)!=string::npos) sumAff+=10;
+  if(str1.length()==str2.length()) sumAff+=2;
+  istringstream iss(str1);
+  do{
+      string sub;
+      iss >> sub;
+      if(str2.find(sub)!=string::npos) sumAff+=3;
+  } while (iss);
+  istringstream iss2(str2);
+  do{
+      string sub;
+      iss2 >> sub;
+      if(str1.find(sub)!=string::npos) sumAff+=3;
+  } while (iss2);
+  return sumAff;
+}
 
 
 int main ()
 {
-  int i=0;
+  int i=0,j=0;
+  int affinityArray[Nmen][Nmen];
   mentions* mentionArray = new mentions[Nmen];
+  entity* entityArray = new entity[Nmen];
   // Andrew MaCallum
   mentionArray[0].token="Andrew McCallum";
   mentionArray[1].token="Andrew MacCallum";
@@ -58,7 +87,23 @@ int main ()
 
   for(i=0;i<Nmen;i++){
     mentionArray[i].entityId=i;
+    entityArray[i].mentions.insert(i);
   }
+  // calcualte the affinity score 
+  for(i=0;i<Nmen;i++){
+    for(j=0;j<Nmen;j++){
+      if(j>i) {
+         int score=affinity(mentionArray[i].token,mentionArray[j].token);
+         affinityArray[i][j]=score;
+         affinityArray[j][i]=score;
+         cout<<mentionArray[i].token<<"	"<<mentionArray[j].token<<"	"<<score<<endl;
+      }
+    }
+  } 
+  //set<int>::iterator it; 
+  //it=entityArray[4].mentions.begin();
+  //for (it=entityArray[4].mentions.begin(); it!=entityArray[4].mentions.end(); ++it)
+  //    cout << " " << *it;
   
   return 0;
 }
