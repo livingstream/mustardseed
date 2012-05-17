d1893fe54aed44bd525d2aa3399ee94c47589d28
Kun Li <kli@cise.ufl.edu>
2012-05-17 16:01:20 -0400

    update

--- mdata.py
+++ mdata.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import re
+import random
 from collections import Counter
 sys.path.append("openpyxl")
 from openpyxl.workbook import Workbook
@@ -11,11 +12,12 @@ testingf=open("testing",'w');
 sheetfinal=wb.get_sheet_by_name(name='final')
 death=0
 alive=0
-topk_token=50
+topk_token=5000
 dic=[]
 cnt = Counter()
 deathf=[]
 alivef=[]
+finaltraindata=[]
 for i in range(2,439):
         value=((sheetfinal.cell('E'+str(i)).value).lower()).strip()
         text=sheetfinal.cell('J'+str(i)).value+sheetfinal.cell('K'+str(i)).value+sheetfinal.cell('L'+str(i)).value
@@ -26,13 +28,13 @@ for i in range(2,439):
            deathf.append(text)
            if death<=175:
                 for word in re.findall(r'\w+',text):
-                    cnt[word] += 1
+                    cnt[word]+=1
         elif value=="no":
              alive=alive+1
              alivef.append(text)
              if alive<=162:
                 for word in re.findall(r'\w+',text):
-                    cnt[word] += 1
+                    cnt[word]+=1
         else:
              print "error"
              break
@@ -41,7 +43,7 @@ feature=[[ "0" for col in range(len(topk_token))] for row in range(225+212)]
 total=0
 for item in topk_token:
        dic.append(item[0])
-print dic
+#print dic
 #feature extraction
 fileinter=0
 for text in deathf:
@@ -61,7 +63,8 @@ for text in deathf:
     fileinter+=1   
 for i in range(0,225):
     if i<175:
-             trainingf.write(str(i) + "\t1\t{" + ",".join(feature[i]) + "}\n");
+             finaltraindata.append(str(i) + "\t1\t{" + ",".join(feature[i]) + "}\n");
+             #trainingf.write(str(i) + "\t1\t{" + ",".join(feature[i]) + "}\n");
     else:
              testingf.write(str(i-175) + "\t1\t{" + ",".join(feature[i]) + "}\n");
 
@@ -81,7 +84,11 @@ for text in alivef:
     fileinter+=1
 for i in range(0,212):
     if i<162:
-             trainingf.write(str(i+175) + "\t2\t{" + ",".join(feature[i-225]) + "}\n");
+             finaltraindata.append(str(i+175) + "\t2\t{" + ",".join(feature[i+225]) + "}\n")
+             #trainingf.write(str(i+175) + "\t2\t{" + ",".join(feature[i+225]) + "}\n");
     else:
-             testingf.write(str(i-162+50) + "\t2\t{" + ",".join(feature[i-225]) + "}\n");
-
+             testingf.write(str(i-162+50) + "\t2\t{" + ",".join(feature[i+225]) + "}\n");
+#write traning data into file
+random.shuffle(finaltraindata)
+for i in range(0,175+162):
+    trainingf.write(finaltraindata[i]);
