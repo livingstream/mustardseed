78accc59a311ea3ad967c507730bfec93486b075
Kun Li <kli@cise.ufl.edu>
2012-05-20 23:37:45 -0400

    update

--- mcmc.cpp
+++ mcmc.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <cstring>
 #include <set>
 #include <ctime>
 #include <cstdlib>
 #include <math.h>
 #include <vector>
+#include <stdio.h>
+/* strlcpy based on OpenBSDs strlcpy */
+#include <sys/types.h>
 using namespace std;
 #define Nmen 29 // number of mentions
 #define Niter 20000 // number of iterations
@@ -21,6 +25,35 @@ struct entity {
 	int id; // An unique identifier for the entity (in consequential)
         set<int>mentions; // all the mentions belong to the entity   
 };
+
+/*
+ * Copy src to string dst of size siz.  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz == 0).
+ * Returns strlen(src); if retval >= siz, truncation occurred.
+ */
+size_t strlcpy(char *dst, const char *src, size_t siz){
+        char *d = dst;
+        const char *s = src;
+        size_t n = siz;
+
+        /* Copy as many bytes as will fit */
+        if (n != 0 && --n != 0) {
+                do {
+                        if ((*d++ = *s++) == 0)
+                                break;
+                } while (--n != 0);
+        }
+        /* Not enough room in dst, add NUL and traverse rest of src */
+        if (n == 0) {
+                if (siz != 0)
+                        *d = '\0';                /* NUL-terminate dst */
+                while (*s++)
+                        ;
+        }
+        return(s - src - 1);        /* count does not include NUL */
+}
+
+
 
 // affinity factor or features
 int affinity(mentions* mention1, mentions* mention2){
