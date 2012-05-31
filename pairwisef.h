d91ecaf49fbaf91eca6848dfc11e0b366f616f95
Kun Li <kli@cise.ufl.edu>
2012-05-31 17:31:11 -0400

    update

--- pairwisef.h
+++ pairwisef.h
@@ -1,45 +1,54 @@
+#include <assert.h>
+#include <cstdlib>
+#include <cstring>
+#include "mcmclib.cpp"
+using namespace std;
+
 class PairwiseF {
 public:
-  char *token;
-  int score(PairwiseF & other){
+  int len;
+  char *token; 
+  PairwiseF(char*);
+  int score(PairwiseF &);
+};
+
+PairwiseF::PairwiseF(char* token){
+   token=token;
+   len=strlen(token);
+   assert (len<=49);//TODO delete it
+}
+
+int PairwiseF::score(PairwiseF & other){
   int sumAff=0;
-  char str1[50]="";
-  char str2[50]="";
-  int str1Len=strlen(mention1->token);
-  int str2Len=strlen(mention2->token);
-  if(str1Len>49||str2Len>49){cout<<"exceed 50 chars, exit!<<endl";cin.get();}
-  strlcpy(str1,mention1->token,str1Len+1);
-  strlcpy(str2,mention2->token,str2Len+1);
-  string s1=str1;
-  string s2=str2;
+  char s1[50]="",s2[50]="";
+  strlcpy(s1,this->token,this->len+1);
+  strlcpy(s2,other.token,other.len+1);
 
-  bool firstMatch=false;
-  bool secondMatch=false;
-  bool thirdMatch=false;
+  s1=(string)s1;
+  s2=(string)s2;
+  bool firstMatch=false, secondMatch=false, thirdMatch=false;
 
-  firstMatch = (str1[0]==str2[0]);
-  if(str1Len>=2&&str2Len>=2) secondMatch = (str1[1]==str2[1]);
-  if(str1Len>=3&&str2Len>=3) thirdMatch = (str1[2]==str2[2]);
+  firstMatch = (s1[0]==s2[0]);
+  secondMatch = (s1[1]==s2[1]);
+  thirdMatch = (s1[2]==s2[2]);
 
   // match the the prefix with 1 character
   firstMatch ? sumAff+=1 : sumAff-=1;
   // match the the prefix with 2 character
-  firstMatch&&secondMatch ? sumAff+=2 : sumAff-=1;
+  firstMatch&secondMatch ? sumAff+=2 : sumAff-=1;
   // match the the prefix with 3 character
-  firstMatch&&secondMatch&&thirdMatch ? sumAff+=3 : sumAff-=0;
-
+  firstMatch&secondMatch&thirdMatch ? sumAff+=3 : sumAff-=0;
   //have the same length  
-  str1Len==str2Len ? sumAff+=3 : sumAff-=0;
+  this.len==other.len ? sumAff+=3 : sumAff-=0;
 
   //one string is a substring of the second string
-  str1Len<=str2Len ? (s2.find(s1)!=string::npos ? sumAff+=10 : sumAff-=1) : (s1.find(s2)!=string::npos ? sumAff+=10 : sumAff-=1);
+  this.len<=other.len ? (s2.find(s1)!=string::npos ? sumAff+=10 : sumAff-=1) : 
+                        (s1.find(s2)!=string::npos ? sumAff+=10 : sumAff-=1);
   const char* split=" ";
-  char *saveptr1=NULL;
-  char *saveptr2=NULL;
-  char *p;
+  char *saveptr1=NULL,*saveptr2=NULL,*p;
 
   //split string into tokens
-  p=strtok_r(str1,split,&saveptr1);
+  p=strtok_r(s1,split,&saveptr1);
   //string sp=p;
   s2.find(p)!=string::npos ? sumAff+=4 : sumAff-=0;
   while(p!=NULL){
@@ -50,7 +59,7 @@ public:
   }
 
   //split string into tokens
-  p=strtok_r(str2,split,&saveptr2);
+  p=strtok_r(s2,split,&saveptr2);
   //sp=p;
   s1.find(p)!=string::npos ? sumAff+=4 : sumAff-=0;
   while(p!=NULL){
@@ -60,5 +69,5 @@ public:
      }
   }
   return sumAff;
+}
 
-};
