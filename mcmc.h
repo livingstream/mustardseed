c2f1731be6e8f587a47dba4e9730161ee9a493bc
Kun Li <kli@cise.ufl.edu>
2012-05-31 18:21:41 -0400

    up

--- mcmc.h
+++ mcmc.h
@@ -4,17 +4,19 @@
 #include "clusterF.h"
 using namespace std;
 struct mentions {
-        char* token; // The actual string
+        char token[50]; // The actual string
         int length;
         int doc; // The identifier for the document (could be a string)
         int para; // The number paragraph in the document
         int word; // The number word in the pargraph
         int pos; // Integer identifier for the part of speech
         int entityId; // entity the mention belongs to
+        PairwiseF pairwiseF;
 };
 
 struct entity {
         int id; // An unique identifier for the entity (in consequential)
         set<int> mentionSet; // all the mentions belong to the entity   
+        ClusterF clusterF;
 };
 
