80af50b8e359bf27ca4840609874b3974b167f9b
Kun Li <kli@cise.ufl.edu>
2012-06-03 00:46:23 -0400

    update

--- mention.h
+++ mention.h
@@ -8,19 +8,63 @@ class prefixFeature {
           char firstC;
           char secondC;
           char thirdC;
-          prefixFeature(char first, char second, char third){
+          void set(char first, char second, char third){
             firstC=first;
             secondC=second;
             thirdC=third;
           }
           int prefixScore(prefixFeature& other){
-            return this->firstC==other.firstC ? (1+(this->secondC==other.secondC ? ((this->thirdC==other.thirdC ? 3 : 0)+2) : -1)): -1
+            return this->firstC==other.firstC ? (1+(this->secondC==other.secondC ? ((this->thirdC==other.thirdC ? 3 : 0)+2) : -1)): -1;
           }
+}
+
+class substrFeature {
+	public:
+          char tokenArray[5][50];
+          int tokenLen[5];
+          char stringL[50];
+          int len;
+          int token_size;
+	  void set(char* tokenS, int length){
+            len=length;
+            int i=0;
+            char** token;
+            memcpy(stringL,tokenS,strlen(tokenS)+1);
+	    this->token_size=size;
+            for(i=0;i<size;i++){
+              memcpy(tokenArray[i],token[i],strlen(token[i])+1);
+            }
+          }
+	  int substrScore(substrFeature& other){
+              int i=0;
+	      //one string is a substring of the second string
+              int sum=this->len>=other.len ? (strstr(this->stringL,other.stringL)==NULL ? -1 : 10) : (strstr(other.stringL,this->stringL)==NULL ? -1 : 10);
+              for (i=0;i<this->token_size;i++){
+              	sum+=this->tokenLen[i]>=other.len ?  (strstr(this->tokenArray[i],other.stringL)==NULL ? -1 : 10) : 
+                                                     (strstr(other.stringL,this->tokenArray[i])==NULL ? -1 : 10);
+              } 
+              for (i=0;i<other.token_size;i++){
+                sum+=this->len>=other.tokenLen[i] ?  (strstr(this->stringL,other.tokenArray[i])==NULL ? -1:10) : 
+                                                     (strstr(other.tokenArray[i],this->stringL)==NULL? -1 : 10);
+              } 
+              return sum;
+	  }
+}
+
+class lengthFeature {
+       public: 
+	int length;
+        void set(int len){
+          this->length=len;
+        }
+        int lengthScore(lengthFeature & other){
+          return this->length==other.length ? 3 : 0;
+        }
 }
 
 class mention {
       public:
-        char token[50]; // The actual string
+        char stringL[50]; // The actual string
         int len;
         int doc; // The identifier for the document (could be a string)
         int para; // The number paragraph in the document
@@ -30,53 +74,21 @@ class mention {
         prefixFeature prefixf;
         substrFeature substrf;
         lengthFeature lengthf;
-        mention(char[] stringL, int length, int doc_id, int para_id, int word_id, int pos_id, int entity_id){
-           this->token=stringL;
+        mention(const char*str, int length, int doc_id, int para_id, int word_id, int pos_id, int entity_id){
+           memcpy(this->stringL,str,strlen(str)+1);
            this->len=length;
            this->doc=doc_id;
            this->para=para_id;
            this->word=word_id;
            this->pos=pos_id;
            this->entityId=entity_id;
+           char temp[4];
+           memcpy(temp,str,3);
+           prefixf.set(temp[0],temp[1],temp[2]);
+           substrf.set(str,length);
+           lengthf.set(length);
         }
         int pairwiseScore(mention& other){
 	  return prefixf.prefixScore(other.prefixf)+substrf.substrScore(other.substrf)+lengthf.lengthScore(other.lengthf);
         }
 };
-
-class substrFeature {
-	public:
-          char tokenArray[5][50];
-          char stringL[50];
-          int len;
-          int token_size;
-	  substrFeature(char* stringL, char** token, int size){
-            this->stringL=stringL;
-	    this->tokenArray=token;
-	    this->token_size=size;
-          }
-	  int substrScore(substrFeature& other){
-              int i=0;
-	      //one string is a substring of the second string
-              int sum=this->len>=other.len ? (strstr(*stringL,other.stringL)==NULL ? -1 : 10) : (strstr(other.stringL,*stringL)==NULL ? -1 : 10);
-              for (i=0;i<this->token_size;i++){
-              	sum+=tokenArray[i]->len>=other.len ? (strstr(*tokenArray[i],other.stringL)==NULL ? -1 : 10) : (strstr(other.stringL,*tokenArray[i])==NULL ? -1 : 10);
-              } 
-              for (i=0;i<other.token_size;i++){
-              sum+=this->len>=other.tokenArray[i] ? (strstr(this->stringL,other.tokenArray[i])==NULL?-1:10) : (strstr(other.tokenArray[i],this->stringL)==NULL? -1 : 10);
-              } 
-              return sum;
-	  }
-}
-
-class lengthFeature {
-       public: 
-	int length;
-        lengthFeature(int len){
-          this->length=len;
-        }
-        int lengthScore(lengthFeature & other){
-          return this->length==other.length ? sumAff+=3 : sumAff-=0;
-        }
-      
-}
