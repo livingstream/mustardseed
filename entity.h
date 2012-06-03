c8f84dc67605c19776062b94ce915cc382cfb420
Kun Li <kli@cise.ufl.edu>
2012-06-03 14:46:23 -0400

    add

--- entity.h
+++ entity.h
@@ -1,15 +1,51 @@
 #include <iostream>
 #include <set>
 using namespace std;
+#define clusterPrefixW (10)
+#define clusterSameDocW (10)
+class clusterPrefixF {
+	public:
+          bool same_prfeix;
+          set<int>prefixSet;
+          int prefix[256];
+          clusterPrefixF(){
+             memset(prefix,0, sizeof(prefix) * sizeof(int));
+          }
+          void add(int char_ascii){
+	  	prefixSet.insert(char_ascii);
+		prefix[char_ascii]++;
+          }
+          void remove(int char_ascii){
+		prefix[char_ascii]--;
+                if(prefix[char_ascii]==0) prefixSet.erase(char_ascii);
+          }
+          int clusterPrefixScore(){
+		return prefixSet.size==1 ? clusterPrefixW : 0;	
+          }
+};
+
+class clusterSameDocF {
+	public:
+	  map<int,int> docMap;
+          void add(int doc_id){
+	    docMap[doc_id]=docMap[doc_id]+1;
+          }
+	  void delete(int doc_id){
+            docMap[doc_id]=docMap[doc_id]-1;
+            if(docMap[doc_id]==0) docMap.erase(doc_id);
+          }
+          int clusterSameDocScore(){
+		return docMap.size()==1 ? clusterSameDocW : 0;	
+          }
+};
 
 class entity {
       public:
         int id; // An unique identifier for the entity (in consequential)
         set<int> mentionSet; // all the mentions belong to the entity   
-        int clusterScore();
+        clusterPrefixF clusterPrefixf;
+        clusterSameDocF clusterSameDocf;
+        int clusterScore(){
+	   return clusterPrefixf.clusterPrefixScore()+clusterSameDoc.clusterSameDocScore();         
+        }
 };
-int entity::clusterScore(){
-
-        return 0;
-}
-
