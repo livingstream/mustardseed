f4cc9c2553881043bf9aa7a4d76e4305080cb10e
Kun Li <kli@cise.ufl.edu>
2012-06-04 11:33:44 -0400

    dd

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <map>
+using namespace std;
+int main ()
+{
+  //char b[100]={'\0'};
+  char b[100];
+  cout<<b[98]<<endl;
+  return 0;
+}
