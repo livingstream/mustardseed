903a773c5f263927eee0bfd033577fa134241282
Kun Li <kli@cise.ufl.edu>
2012-06-20 11:39:55 -0400

    change niter nmention locatio to mention.h

--- corefrence/mcmc.cpp
+++ corefrence/mcmc.cpp
@@ -14,13 +14,10 @@
 #include <assert.h>
 #include "entity.h"
 using namespace std;
-#define Nmen 300000 // number of mentions
-//#define Nmen 3000 // number of mentions
-#define Niter 20000000 // number of iterations
 #define bias 0 // affinity score and replusion score bais
 //#define nytdatapath "/home/cgrant/data/NYT/dbdump/nytmentionsfull_collapsed.csv"
 #define nytdatapath "/home/kun/Desktop/nytmentionspy.csv"
-#define DEBUG false
+#define DEBUG true
 
 entity entityArray[Nmen];
 
