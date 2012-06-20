2447de4af7eb37e3d669eae7a249d15f69a8697b
Kun Li <kli@cise.ufl.edu>
2012-06-20 10:35:11 -0400

    format the code using astyle

--- corefrence/entity.h
+++ corefrence/entity.h
@@ -8,82 +8,82 @@ using namespace std;
 mention mentionArray[Nmen];
 
 class clusterPrefixF {
-	public:
-          bool same_prfeix;
-          unordered_set<size_t>prefixSet;
-          int prefix[256];
-          clusterPrefixF(){
-             memset(prefix,0, sizeof(prefix) * sizeof(int));
-          }
-          void add(int char_ascii){
-	  	prefixSet.insert(char_ascii);
-		prefix[char_ascii]++;
-          }
-          void remove(int char_ascii){
-		prefix[char_ascii]--;
-                if(prefix[char_ascii]==0) prefixSet.erase(char_ascii);
-          }
-          int clusterPrefixScore(){
-		return prefixSet.size()==1 ? clusterPrefixW : 0;	
-          }
+public:
+    bool same_prfeix;
+    unordered_set<size_t>prefixSet;
+    int prefix[256];
+    clusterPrefixF() {
+        memset(prefix,0, sizeof(prefix) * sizeof(int));
+    }
+    void add(int char_ascii) {
+        prefixSet.insert(char_ascii);
+        prefix[char_ascii]++;
+    }
+    void remove(int char_ascii) {
+        prefix[char_ascii]--;
+        if(prefix[char_ascii]==0) prefixSet.erase(char_ascii);
+    }
+    int clusterPrefixScore() {
+        return prefixSet.size()==1 ? clusterPrefixW : 0;
+    }
 };
 
 class clusterSameDocF {
-	public:
-	  unordered_map<size_t,size_t> docMap;
-          void add(int doc_id){
-	    docMap[doc_id]=docMap[doc_id]+1;
-          }
-	  void remove(size_t doc_id){
-            docMap[doc_id]=docMap[doc_id]-1;
-            if(docMap[doc_id]==0) docMap.erase(doc_id);
-          }
-          int clusterSameDocScore(){
-		return docMap.size()==1 ? clusterSameDocW : 0;	
-          }
+public:
+    unordered_map<size_t,size_t> docMap;
+    void add(int doc_id) {
+        docMap[doc_id]=docMap[doc_id]+1;
+    }
+    void remove(size_t doc_id) {
+        docMap[doc_id]=docMap[doc_id]-1;
+        if(docMap[doc_id]==0) docMap.erase(doc_id);
+    }
+    int clusterSameDocScore() {
+        return docMap.size()==1 ? clusterSameDocW : 0;
+    }
 };
 
 struct token_count {
-	char* token[maxtokenlen];
-        size_t count;
+    char* token[maxtokenlen];
+    size_t count;
 };
 
 class entity {
-      public:
-        size_t id; // An unique identifier for the entity (in consequential)
-        unordered_set<size_t> mentionSet; // all the mentions belong to the entity   
-        unordered_set<size_t> othersmentionSet; // all the mentions belong to the entity   
-        token_count token_freq[group_size];
-        unordered_set<size_t> group_set[group_size];
-        clusterPrefixF clusterPrefixf;
-        clusterSameDocF clusterSameDocf;
-        void insert(int mentionId){
-                bool found=false;
-        	mentionSet.insert(mentionId); 
-                for(int i=0;i<group_size;i++){
-			if(token_freq[i].count==0){
-                           token_freq[i].count=1;
-                           memcpy(token_freq[i].token,mentionArray[mentionId].stringL,strlen(mentionArray[mentionId].stringL));
-                           group_set[i].insert(mentionId);
-                           found=true;
-                           break;
-                        } else if (memcmp(mentionArray[mentionId].stringL,token_freq[i].token,maxtokenlen-1)==0){
-		           token_freq[i].count++;
-                           group_set[i].insert(mentionId);
-			   found=true;
-                           break;
-                        }
-                }
-                if(!found) othersmentionSet.insert(mentionId);
+public:
+    size_t id; // An unique identifier for the entity (in consequential)
+    unordered_set<size_t> mentionSet; // all the mentions belong to the entity
+    unordered_set<size_t> othersmentionSet; // all the mentions belong to the entity
+    token_count token_freq[group_size];
+    unordered_set<size_t> group_set[group_size];
+    clusterPrefixF clusterPrefixf;
+    clusterSameDocF clusterSameDocf;
+    void insert(int mentionId) {
+        bool found=false;
+        mentionSet.insert(mentionId);
+        for(int i=0; i<group_size; i++) {
+            if(token_freq[i].count==0) {
+                token_freq[i].count=1;
+                memcpy(token_freq[i].token,mentionArray[mentionId].stringL,strlen(mentionArray[mentionId].stringL));
+                group_set[i].insert(mentionId);
+                found=true;
+                break;
+            } else if (memcmp(mentionArray[mentionId].stringL,token_freq[i].token,maxtokenlen-1)==0) {
+                token_freq[i].count++;
+                group_set[i].insert(mentionId);
+                found=true;
+                break;
+            }
         }
-        int clusterScore(){
-	   return clusterPrefixf.clusterPrefixScore()+clusterSameDocf.clusterSameDocScore();         
+        if(!found) othersmentionSet.insert(mentionId);
+    }
+    int clusterScore() {
+        return clusterPrefixf.clusterPrefixScore()+clusterSameDocf.clusterSameDocScore();
+    }
+    entity() {
+        id=0;
+        for(int i=0; i<group_size; i++) {
+            memset(token_freq[i].token,'\0',maxtokenlen);
+            token_freq[i].count=0;
         }
-        entity(){
-	  id=0;
-          for(int i=0; i<group_size; i++){
-		memset(token_freq[i].token,'\0',maxtokenlen);
-		token_freq[i].count=0;
-          }
-        }
+    }
 };
--- corefrence/mention.h
+++ corefrence/mention.h
@@ -10,129 +10,141 @@ using namespace std;
 #define Niter 20000000 // number of iterations
 
 class prefixFeature {
-	public:
-          char firstC;
-          char secondC;
-          char thirdC;
-          prefixFeature(){
-	    firstC='\0'; secondC='\0'; thirdC='\0';
-          }
-          void set(char first, char second, char third){
-            firstC=first;
-            secondC=second;
-            thirdC=third;
-          }
-          int prefixScore(prefixFeature& other){
-            return firstC==other.firstC ? (1+(secondC==other.secondC ? ((thirdC==other.thirdC ? 3 : 0)+2) : -1)): -1;
-          }
+public:
+    char firstC;
+    char secondC;
+    char thirdC;
+    prefixFeature() {
+        firstC='\0';
+        secondC='\0';
+        thirdC='\0';
+    }
+    void set(char first, char second, char third) {
+        firstC=first;
+        secondC=second;
+        thirdC=third;
+    }
+    int prefixScore(prefixFeature& other) {
+        return firstC==other.firstC ? (1+(secondC==other.secondC ? ((thirdC==other.thirdC ? 3 : 0)+2) : -1)): -1;
+    }
 };
 
 class substrFeature {
-	public:
-          static unordered_map<string,int> tokenDict;
-          char tokenArray[maxtoken][maxtokenlen];
-          int tokenIntArray[maxtoken];
-          char stringL[maxtokenlen];
-          int len;
-          int token_size;
-          substrFeature(){
-            memset(stringL,'\0',maxtokenlen);
-            for(int i=0;i<maxtoken;i++){
-            	tokenIntArray[i]=0;
-                memset(tokenArray[i],'\0',maxtokenlen);
-            }
-            len=0;
-            token_size=0;
-          }
-	  void set(char* tokenS, size_t length);
-	  int substrScore(substrFeature& other);
+public:
+    static unordered_map<string,int> tokenDict;
+    char tokenArray[maxtoken][maxtokenlen];
+    int tokenIntArray[maxtoken];
+    char stringL[maxtokenlen];
+    int len;
+    int token_size;
+    substrFeature() {
+        memset(stringL,'\0',maxtokenlen);
+        for(int i=0; i<maxtoken; i++) {
+            tokenIntArray[i]=0;
+            memset(tokenArray[i],'\0',maxtokenlen);
+        }
+        len=0;
+        token_size=0;
+    }
+    void set(char* tokenS, size_t length);
+    int substrScore(substrFeature& other);
 };
 
-unordered_map<string,int> substrFeature::tokenDict({{string("NULL"),0}, {string("NULL"),0}}); // Try to initialize it.
+unordered_map<string,int> substrFeature::tokenDict( {{string("NULL"),0}, {string("NULL"),0}}); // Try to initialize it.
 
-void substrFeature::set(char* tokenS, size_t length){
-	memcpy(stringL,tokenS,strlen(tokenS)+1);
-	len=length;
-	char* token = strtok(tokenS, " ");
-	//cout<<token<<endl;
-	//cout<<"size: "<<tokenDict.size()<<endl;
-	size_t wordindex=0;
-	if(tokenDict.count((string)token)==0){
-		//cout<<"set21"<<endl;
-		wordindex=substrFeature::tokenDict.size();
-		substrFeature::tokenDict.insert(pair<string,size_t>(token,wordindex));
-	} else {
-		wordindex=substrFeature::tokenDict.find(token)->second;
-	}
-	tokenIntArray[0]=wordindex;
-	memcpy(tokenArray[0],token,min(maxtokenlen,(int)strlen(token)+1));
-	token_size=1;
-	while (token_size<maxtoken && token) {
-		token = strtok(NULL, " ");
-		if(token){
-			int wordindex=0;
-			if(substrFeature::tokenDict.count(token)==0){
-				wordindex=substrFeature::tokenDict.size();
-				substrFeature::tokenDict.insert(pair<string,size_t>(token,wordindex));
-			} else {
-				wordindex=substrFeature::tokenDict.find(token)->second;
-			}
-			tokenIntArray[token_size]=wordindex;
+void substrFeature::set(char* tokenS, size_t length) {
+    memcpy(stringL,tokenS,strlen(tokenS)+1);
+    len=length;
+    char* token = strtok(tokenS, " ");
+    //cout<<token<<endl;
+    //cout<<"size: "<<tokenDict.size()<<endl;
+    size_t wordindex=0;
+    if(tokenDict.count((string)token)==0) {
+        //cout<<"set21"<<endl;
+        wordindex=substrFeature::tokenDict.size();
+        substrFeature::tokenDict.insert(pair<string,size_t>(token,wordindex));
+    } else {
+        wordindex=substrFeature::tokenDict.find(token)->second;
+    }
+    tokenIntArray[0]=wordindex;
+    memcpy(tokenArray[0],token,min(maxtokenlen,(int)strlen(token)+1));
+    token_size=1;
+    while (token_size<maxtoken && token) {
+        token = strtok(NULL, " ");
+        if(token) {
+            int wordindex=0;
+            if(substrFeature::tokenDict.count(token)==0) {
+                wordindex=substrFeature::tokenDict.size();
+                substrFeature::tokenDict.insert(pair<string,size_t>(token,wordindex));
+            } else {
+                wordindex=substrFeature::tokenDict.find(token)->second;
+            }
+            tokenIntArray[token_size]=wordindex;
 
-			memcpy(substrFeature::tokenArray[token_size],token,min(maxtokenlen,(int)strlen(token)+1));
-			token_size++;
-		}
-	}
+            memcpy(substrFeature::tokenArray[token_size],token,min(maxtokenlen,(int)strlen(token)+1));
+            token_size++;
+        }
+    }
 }
 
-int substrFeature::substrScore(substrFeature& other){
-	int sum=len>=other.len ? (strstr(stringL,other.stringL)==NULL ? -1 : 10) : (strstr(other.stringL,stringL)==NULL ? -1 : 10);
-	for(int i=0;i<token_size;i++)
-		for(int j=0;j<other.token_size;j++)
-			sum+=(tokenIntArray[i]!=other.tokenIntArray[j] ? -1:10);
-	return sum;
+int substrFeature::substrScore(substrFeature& other) {
+    int sum=len>=other.len ? (strstr(stringL,other.stringL)==NULL ? -1 : 10) : (strstr(other.stringL,stringL)==NULL ? -1 : 10);
+    for(int i=0; i<token_size; i++)
+        for(int j=0; j<other.token_size; j++)
+            sum+=(tokenIntArray[i]!=other.tokenIntArray[j] ? -1:10);
+    return sum;
 }
 
 class lengthFeature {
-       public: 
-	size_t length;
-        lengthFeature(){
-	  length=0;
-        }
-        void set(size_t len){
-          length=len;
-        }
-        int lengthScore(lengthFeature & other){
-          return length==other.length ? 3 : 0;
-        }
+public:
+    size_t length;
+    lengthFeature() {
+        length=0;
+    }
+    void set(size_t len) {
+        length=len;
+    }
+    int lengthScore(lengthFeature & other) {
+        return length==other.length ? 3 : 0;
+    }
 };
 
 class mention {
-      public:
-        char stringL[maxtokenlen]; // The actual string
-        int len;
-        int doc; // The identifier for the document (could be a string)
-        int para; // The number paragraph in the document
-        int word; // The number word in the pargraph
-        int pos; // Integer identifier for the part of speech
-        int entityId; // entity the mention belongs to
-        prefixFeature prefixf;
-        substrFeature substrf;
-        lengthFeature lengthf;
-        mention(){
-          memset(stringL,'\0',maxtokenlen);
-          len=0;doc=0;para=0;word=0;pos=0;entityId=0;
-        }
-        void set(char*str, int length, int doc_id, int para_id, int word_id, int pos_id, int entity_id){
-           memcpy(stringL,str,min(maxtokenlen-1,(int)strlen(str)));
-           len=length; doc=doc_id; para=para_id; word=word_id; pos=pos_id; entityId=entity_id;
-           char temp[3]={'\0'};
-           memcpy(temp,str,min(3,(int)strlen(str)));
-           prefixf.set(temp[0],temp[1],temp[2]);
-           substrf.set(str,length);
-           lengthf.set(length);
-        }
-        int pairwiseScore(mention& other){
-	  return prefixf.prefixScore(other.prefixf)+substrf.substrScore(other.substrf)+lengthf.lengthScore(other.lengthf);
-        }
+public:
+    char stringL[maxtokenlen]; // The actual string
+    int len;
+    int doc; // The identifier for the document (could be a string)
+    int para; // The number paragraph in the document
+    int word; // The number word in the pargraph
+    int pos; // Integer identifier for the part of speech
+    int entityId; // entity the mention belongs to
+    prefixFeature prefixf;
+    substrFeature substrf;
+    lengthFeature lengthf;
+    mention() {
+        memset(stringL,'\0',maxtokenlen);
+        len=0;
+        doc=0;
+        para=0;
+        word=0;
+        pos=0;
+        entityId=0;
+    }
+    void set(char*str, int length, int doc_id, int para_id, int word_id, int pos_id, int entity_id) {
+        memcpy(stringL,str,min(maxtokenlen-1,(int)strlen(str)));
+        len=length;
+        doc=doc_id;
+        para=para_id;
+        word=word_id;
+        pos=pos_id;
+        entityId=entity_id;
+        char temp[3]= {'\0'};
+        memcpy(temp,str,min(3,(int)strlen(str)));
+        prefixf.set(temp[0],temp[1],temp[2]);
+        substrf.set(str,length);
+        lengthf.set(length);
+    }
+    int pairwiseScore(mention& other) {
+        return prefixf.prefixScore(other.prefixf)+substrf.substrScore(other.substrf)+lengthf.lengthScore(other.lengthf);
+    }
 };
