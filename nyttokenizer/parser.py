d602572a28b49821d1ad57642fe61edc061e31ff
Kun Li <kli@cise.ufl.edu>
2012-12-04 22:11:01 -0500

    update

--- nyttokenizer/parser.py
+++ nyttokenizer/parser.py
@@ -1,18 +1,33 @@
-f=open("/home/kun/nytdataset/nyt_eng/nyt_eng_199407")
-output=open("/home/kun/mustardseed/nyttokenizer/nyt_eng_tokenized",'w')
+import os
+import nltk.data
+path="/home/kun/nytdataset/nyt_eng"
+output=open("/home/kun/mustardseed/nyttokenizer/nyt_eng_sentenced",'w')
 pindicator=False
 linecount=0
 oneline=""
-for line in f:
-    line=line.lower().strip()
-    if line.find("</p>") != -1:
-       linecount=linecount+1
-       output.write(oneline.strip()+"\n");
-       print oneline
-       pindicator = False
-    if pindicator == True:
-       oneline=oneline+" "+line
-    if line.find("<p>") != -1:
-       pindicator = True
-       oneline=""
-print linecount
+flag=False
+listing = os.listdir(path)
+for infile in listing:
+        print "filename="+infile +" count="+str(linecount)
+        if flag:
+           break
+        f=open(path+"/"+infile)
+	for line in f:
+	    line=line.lower().strip()
+	    if line.find("</p>") != -1:
+               tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+               senArray= tokenizer.tokenize(oneline)
+	       pindicator = False
+               linecount=linecount+len(senArray)
+               for sen in senArray:
+                   output.write(sen.strip()+"\n")
+               if(linecount>8000050):
+               #if(linecount>50):
+                 flag=True
+                 break
+	    if pindicator == True:
+	       oneline=oneline+" "+line.strip()
+	    if line.find("<p>") != -1:
+	       pindicator = True
+               oneline=""
+        f.close()
